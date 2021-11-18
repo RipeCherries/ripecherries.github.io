a7400102f1e782e21f61ad61fe2268986b7aedf4
Vl1ts <78637094+Vl1ts@users.noreply.github.com>
2021-11-18 21:15:31 +0300

    Create catalog filters

--- js/main.js
+++ js/main.js
@@ -25,4 +25,19 @@ $(function(){
         prevArrow: '<button class="banner-section__slider-btn product-slider__slider-btnprev"><img src="images/arrow-black-left.svg" alt="arrow-left"></button>',
         nextArrow: '<button class="banner-section__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt="arrow-right"></button>'
     });
+
+    $('.filter-style').styler();
+
+    $('.filter__item-drop, .filter__extra').on('click', function(){
+        $(this).toggleClass('filter__item-drop--active');
+        $(this).next().slideToggle('200');
+    });
+
+    
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 100000,
+        max: 500000,
+    });
+
 });
