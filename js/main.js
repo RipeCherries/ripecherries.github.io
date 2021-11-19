e308deb4a85ad76fa1b9853bb541a5c6714549f3
RipeCherries <alexeyegoshin0403@mail.ru>
2021-11-19 15:11:54 +0700

    Create link

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
     $('.banner-section__slider').slick({
-        dots: true,
+        dots: false,
         prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
         nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt="arrow-right"></button>'
     });
